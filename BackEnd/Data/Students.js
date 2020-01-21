add97133deb13d66192babdc7a258a63a5f933b3
Jayna-Menard <31700864+JaynaMMenard@users.noreply.github.com>
2020-01-20 23:17:18 -0500

    BackEndFinal+

---
+++ BackEnd/Data/Students.js
@@ -0,0 +1,38 @@
+// Hardcoding the students to view the CSS and check if it database entry
+
+let students = [
+    {
+        name: "Harry Potter",
+        img: 'https://s3.amazonaws.com/tinycards/image/fb7b32ea72330a9c944c788eed9bcd05',
+        gpa: 3.8,
+        college: 1
+    },
+    {
+        name: "Hermione Granger",
+        img: 'https://d29fhpw069ctt2.cloudfront.net/icon/image/49320/preview.svg',
+        gpa: 4.0,
+        college: 1
+     },
+     {
+        name: "Ronald Weasley",
+        img: 'https://d29fhpw069ctt2.cloudfront.net/icon/image/49320/preview.svg',
+        gpa: 2.5,
+        college: 1
+     },
+    
+     {
+        name: "Fred and George",
+        img: 'https://d29fhpw069ctt2.cloudfront.net/icon/image/49320/preview.svg',
+        gpa: 2.2,
+        college: 1
+     },
+     
+     
+     
+     
+     
+
+     
+];
+
+module.exports = students;
---
+++ BackEnd/Data/campuses.js
@@ -0,0 +1,31 @@
+// Hardcoding the houses to view the CSS and check if it database entry
+
+let campuses = [
+	{
+		name: "Gryffindor",
+		bio: "Knowledge guides our instincts.",
+		address: "123 Main St",
+		img: "https://wallpaperaccess.com/full/864828.jpg"},
+	{
+		name: "Ravenclaw",
+		bio: "Knowledge comes first.",
+		address: "456 Main St",
+		img: "https://www.itl.cat/pngfile/big/28-285518_ravenclaw-crest.jpg"
+	},
+
+	{
+		name: "Hufflepuff",
+		bio: "Knowledge is gained through hard work.",
+		address: "456 Main St",
+		img: "https://cdn.wallpapersafari.com/63/74/iKtdkm.jpg"
+	},
+
+	{
+		name: "Slytherin",
+		bio: "Knowledge is power.",
+		address: "456 Main St",
+		img: "https://cdn.wallpapersafari.com/9/30/WwGkUu.jpg"
+	}
+];
+
+module.exports = campuses;
