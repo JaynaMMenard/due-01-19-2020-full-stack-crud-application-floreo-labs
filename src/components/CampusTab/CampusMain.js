87638992bd2a068d14e1875df49790a6b28bd8b2
rellis917 <34964868+rellis917@users.noreply.github.com>
2020-01-20 21:01:42 -0500

    new commit

---
+++ src/components/CampusTab/CampusMain.js
@@ -0,0 +1,22 @@
+import React from "react";
+import CampusGrid from "./CampusGrid.js";
+import "./CampusMain.css";
+import {Link} from "react-router-dom";
+
+class CampusMain extends React.Component {
+    render() {
+        return (
+            <div>
+              <div className="campus-main">
+                <div className="campus-nav">
+                 
+                  <Link to="/campusAddForm" className="campus-nav-element">Add Campus</Link>
+                </div>
+                <CampusGrid campuses={this.props.campuses}/>
+              </div>
+            </div>
+        );
+    }
+}
+
+export default CampusMain;
